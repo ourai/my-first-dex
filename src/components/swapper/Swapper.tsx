c04083875fa426c4338da334414fe14258c4bf0e
Ourai Lin <ourairyu@gmail.com>
2024-07-01 22:31:19 +0800

    feat: debounce price request

--- src/components/swapper/Swapper.tsx
+++ src/components/swapper/Swapper.tsx
@@ -1,20 +1,20 @@
+import debounce from 'lodash.debounce'
 import { useState, useEffect } from 'react'
 import { Button, Flex, Spin } from 'antd'
-import { mainnet } from 'wagmi/chains'
+import { useAccount } from 'wagmi'
 import { SwapOutlined } from '@ant-design/icons'
 import { CryptoInput } from '@ant-design/web3'
-import { ETH, USDT } from '@ant-design/web3-assets'
 
+import { TOKEN_LIST } from '../../helper'
 import type { CryptoValue } from './typing'
 import { isCryptoValid } from './helper'
 import { getSwappedTokenPair } from './repository'
 import style from './style.module.scss'
 
-const TOKEN_LIST = [ETH, USDT]
-
 function Swapper() {
   const [tokenPair, setTokenPair] = useState<CryptoValue[]>([{}, {}])
   const [swapping, setSwapping] = useState<boolean>(false)
+  const { chainId, address } = useAccount()
 
   useEffect(() => {
     if (!swapping) {
@@ -27,22 +27,13 @@ function Swapper() {
       return setSwapping(false)
     }
 
-    getSwappedTokenPair(mainnet.id, tokenPair).then(pair => {
-      setTokenPair(pair)
-    }).finally(() => setSwapping(false))
-
-    console.log('use effect', tokenPair)
+    getSwappedTokenPair(chainId!, tokenPair).then(pair => setTokenPair(pair)).finally(() => setSwapping(false))
   }, [tokenPair])
 
-  const changeTokenPair = (pair: CryptoValue[]) => {
+  const changeTokenPair = debounce((pair: CryptoValue[]) => {
     setSwapping(true)
     setTokenPair(pair)
-  }
-
-  const handleSwap = () => {
-    setSwapping(true)
-    setTokenPair([tokenPair[1], tokenPair[0]])
-  }
+  }, 500)
 
   const handleSubmit = () => {
     console.log(tokenPair)
@@ -51,18 +42,22 @@ function Swapper() {
   return (
     <Spin spinning={swapping}>
       <Flex className={style.Swapper} align="center" gap={16} vertical>
-        <CryptoInput value={tokenPair[0]} footer={false} tokenList={TOKEN_LIST} onChange={crypto => changeTokenPair([crypto, tokenPair[1]])} />
-        <span className={style['Swapper-swapButton']} onClick={handleSwap}><SwapOutlined className={style['Swapper-swapIcon']} /></span>
-        <CryptoInput value={tokenPair[1]} footer={false} tokenList={TOKEN_LIST} onChange={crypto => changeTokenPair([tokenPair[0], crypto])} />
-        <Button
-          className={style['Swapper-confirmButton']}
-          type="primary"
-          size="large"
-          disabled={!tokenPair[0]?.amount || !tokenPair[1]?.amount}
-          onClick={handleSubmit}
-        >
-          Swap
-        </Button>
+        {chainId && address ? (
+          <>
+            <CryptoInput value={tokenPair[0]} footer={false} tokenList={TOKEN_LIST} onChange={crypto => changeTokenPair([crypto, tokenPair[1]])} />
+            <span className={style['Swapper-swapButton']} onClick={() => changeTokenPair([tokenPair[1], tokenPair[0]])}><SwapOutlined className={style['Swapper-swapIcon']} /></span>
+            <CryptoInput value={tokenPair[1]} footer={false} tokenList={TOKEN_LIST} onChange={crypto => changeTokenPair([tokenPair[0], crypto])} />
+            <Button
+              className={style['Swapper-confirmButton']}
+              type="primary"
+              size="large"
+              disabled={!tokenPair[0]?.amount || !tokenPair[1]?.amount}
+              onClick={handleSubmit}
+            >
+              Swap
+            </Button>
+          </>
+        ) : <div>Please connect wallet first.</div>}
       </Flex>
     </Spin>
   )
